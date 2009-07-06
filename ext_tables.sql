b94af4d03b74cfa1f0a52b39225ef4834d75fa63
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-06 19:35:53 +0000

    Added field

--- ext_tables.sql
+++ ext_tables.sql
@@ -59,6 +59,7 @@ CREATE TABLE tx_addresses_domain_model_addressgroup (
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
+	upuser_id int(11) unsigned DEFAULT '0' NOT NULL,
 	sorting int(10) DEFAULT '0' NOT NULL,
 	deleted tinyint(4) DEFAULT '0' NOT NULL,
 	hidden tinyint(4) DEFAULT '0' NOT NULL,

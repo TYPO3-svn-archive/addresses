a52527d752616c00a7e82bdf065d08ddadba2628
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-08 13:04:23 +0000

    [BUG] #6437: Update / correct TCA added domain image (e.g. added field upuser_id)

--- ext_tables.sql
+++ ext_tables.sql
@@ -79,6 +79,7 @@ CREATE TABLE tx_addresses_domain_model_tag (
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
+	upuser_id int(11) DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
 	l18n_parent int(11) DEFAULT '0' NOT NULL,
 	l18n_diffsource mediumblob NOT NULL,
